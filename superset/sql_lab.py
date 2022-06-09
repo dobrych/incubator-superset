19a4c6f93b4c18370e7b566dafd756bfc4121aa0
Beto Dealmeida <roberto@dealmeida.net>
2022-06-08 19:17:56 -0700

    Fix lint

--- superset/sql_lab.py
+++ superset/sql_lab.py
@@ -192,7 +192,8 @@ def get_sql_results(  # pylint: disable=too-many-arguments
                 return handle_query_error(ex, query, session)
 
 
-def execute_sql_statement(  # pylint: disable=too-many-arguments,too-many-statements
+# pylint: disable=too-many-arguments,too-many-statements, too-many-locals
+def execute_sql_statement(
     sql_statement: str,
     query: Query,
     session: Session,
